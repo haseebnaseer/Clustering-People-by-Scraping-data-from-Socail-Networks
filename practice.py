a9cf7558fe3abada846cde491f48a77c4b519b45
Haseebnaseer <58005718+haseebnaseer@users.noreply.github.com>
2019-11-30 23:45:52 +0500

    practiceTechnology

---
+++ practice.py
@@ -0,0 +1,28 @@
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+print("_________________________________________")
+txt = "The rain in Spain stays mainly in the plain"
+x = "ain" in txt
+print(x)
+print("----------------------------------------")
+x = 5
+
+x ^= 3
+
+print(x)
+print("----------------------------------------")
+thislist = ["apple", "banana", "Mango"]
+
+thislist.append("orange")
+
+print(thislist)
+print("-----------------------------------------")
+thislist = ["apple", "mango", "cherry"]
+thislist.sort()
+print(thislist)
+print("-----------------------------------------")
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
